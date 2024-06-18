ab9333d8a55adcbb68a0034b8384db6b39ece53f
Alexander Watson <zredlined@users.noreply.github.com>
2024-06-18 09:12:53 -0700

    ux improvements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import requests
 import time
 from io import StringIO
 
@@ -8,8 +9,7 @@ import streamlit as st
 from datasets import load_dataset
 from gretel_client import Gretel
 
-from navigator_helpers import (DataAugmentationConfig, DataAugmenter,
-                               StreamlitLogHandler)
+from navigator_helpers import DataAugmentationConfig, DataAugmenter, StreamlitLogHandler
 
 # Create a StringIO buffer to capture the logging output
 log_buffer = StringIO()
@@ -24,6 +24,7 @@ logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
 
+SAMPLE_DATASET_URL = "https://gretel-public-website.s3.us-west-2.amazonaws.com/datasets/llm-training-data/dolly-examples-qa-with-context.csv"
 WELCOME_MARKDOWN = """Navigator Self Align is an interface for creating high quality, diverse training data examples via synthetic data generation. It leverages techniques such as diverse instruction and response generation, and an iterative AI alignment process including AI-Aligning-AI methodology (AAA) to iteratively enhance data quality. The system integrates multiple LLMs in a co-teaching and self-improvement process, generating diverse and high-quality synthetic instructions, responses, and leverages evaluations for quality, adherence, toxicity, and bias.
 
 ## ✅ What You Need
@@ -64,15 +65,13 @@ def main():
     st.subheader("Step 1: API Key Validation")
     with st.expander("API Key Configuration", expanded=True):
         api_key = st.text_input(
-            "Enter your Gretel API key",
+            "Enter your Gretel API key (https://console.gretel.ai)",
             value="",
             type="password",
             help="Your Gretel API key for authentication",
         )
-
         if "gretel" not in st.session_state:
             st.session_state.gretel = None
-
         if st.button("Validate API Key"):
             if api_key:
                 try:
@@ -82,7 +81,6 @@ def main():
                     st.error(f"Error connecting to Gretel: {str(e)}")
             else:
                 st.warning("Please enter your Gretel API key to proceed.")
-
         if st.session_state.gretel is None:
             st.stop()
 
@@ -90,8 +88,12 @@ def main():
     with st.expander("Data Source", expanded=True):
         data_source = st.radio(
             "Select data source",
-            options=["Upload a file", "Select a dataset from Hugging Face"],
-            help="Choose whether to upload a file or select a dataset from Hugging Face",
+            options=[
+                "Upload a file",
+                "Select a dataset from Hugging Face",
+                "Use a sample dataset",
+            ],
+            help="Choose whether to upload a file, select a dataset from Hugging Face, or use a sample dataset",
         )
 
         df = None
@@ -139,6 +141,15 @@ def main():
                 else:
                     st.warning("Please provide a Hugging Face dataset repository name.")
 
+        elif data_source == "Use a sample dataset":
+            st.write("Try a sample dataset to get started quickly.")
+            if st.button("Try Sample Dataset"):
+                try:
+                    df = pd.read_csv(SAMPLE_DATASET_URL)
+                    st.success("Sample dataset loaded successfully.")
+                except Exception as e:
+                    st.error(f"Error downloading sample dataset: {str(e)}")
+
         if df is not None:
             st.session_state.df = df
             st.session_state.selected_fields = list(df.columns)
@@ -169,11 +180,12 @@ def main():
 
             st.session_state.selected_fields = selected_fields
 
-        with st.expander("Output Fields Configuration", expanded=True):
+        with st.expander("Advanced Options", expanded=False):
+
             output_instruction_field = st.text_input(
                 "Synthetic instruction field",
                 value=st.session_state.get(
-                    "output_instruction_field", "generated_instruction"
+                    "output_instruction_field", "instruction"
                 ),
                 help="Specify the name of the output field for generated instructions",
             )
@@ -182,7 +194,7 @@ def main():
             output_response_field = st.text_input(
                 "Synthetic response field",
                 value=st.session_state.get(
-                    "output_response_field", "generated_response"
+                    "output_response_field", "response"
                 ),
                 help="Specify the name of the output field for generated responses",
             )
@@ -197,9 +209,8 @@ def main():
             )
             st.session_state.num_records = num_records
 
-        with st.expander("Advanced Options", expanded=False):
             num_instructions = st.number_input(
-                "Number of instructions",
+                "Number of diverse candidate instructions",
                 min_value=1,
                 value=st.session_state.get("num_instructions", 5),
                 help="Specify the number of instructions to generate",
@@ -207,7 +218,7 @@ def main():
             st.session_state.num_instructions = num_instructions
 
             num_responses = st.number_input(
-                "Number of responses",
+                "Number of diverse candidateresponses",
                 min_value=1,
                 value=st.session_state.get("num_responses", 5),
                 help="Specify the number of responses to generate",
@@ -224,30 +235,27 @@ def main():
             )
             st.session_state.temperature = temperature
 
-            max_tokens_instruction = st.number_input(
+            max_tokens_instruction = st.slider(
                 "Max tokens (instruction)",
                 min_value=1,
+                max_value=1024,
                 value=st.session_state.get("max_tokens_instruction", 100),
                 help="Specify the maximum number of tokens for instructions",
             )
             st.session_state.max_tokens_instruction = max_tokens_instruction
 
-            max_tokens_response = st.number_input(
+            max_tokens_response = st.slider(
                 "Max tokens (response)",
                 min_value=1,
-                value=st.session_state.get("max_tokens_response", 150),
+                max_value=1024,
+                value=st.session_state.get("max_tokens_response", 100),
                 help="Specify the maximum number of tokens for responses",
             )
             st.session_state.max_tokens_response = max_tokens_response
 
-            use_aaa = st.checkbox(
-                "Use AI Align AI (AAA)",
-                value=st.session_state.get("use_aaa", True),
-                help="Enable or disable the use of AI Align AI. Note: AAA improves output quality by iteratively improving output with multiple LLMs, but reduces synthetic data generation speed.",
-            )
-            st.session_state.use_aaa = use_aaa
-
         with st.expander("Model Configuration", expanded=True):
+            st.markdown("### Primary Navigator Models")
+
             tabular_models = st.session_state.gretel.factories.get_navigator_model_list(
                 "tabular"
             )
@@ -255,7 +263,7 @@ def main():
                 "Navigator Tabular",
                 options=tabular_models,
                 index=st.session_state.get("navigator_tabular_index", 0),
-                help="Select the tabular model to use for navigation",
+                help="Select the primary Navigator tabular model",
             )
             st.session_state.navigator_tabular_index = tabular_models.index(
                 navigator_tabular
@@ -268,25 +276,42 @@ def main():
                 "Navigator LLM",
                 options=nl_models,
                 index=st.session_state.get("navigator_llm_index", 0),
-                help="Select the language model to use for navigation",
+                help="Select the primary Navigator LLM",
             )
             st.session_state.navigator_llm_index = nl_models.index(navigator_llm)
 
-            st.markdown("Navigator Co-teaching LLMs")
+            st.markdown("---")
+            st.markdown("### AI Align AI (AAA)")
             st.write(
-                "Select additional language models for co-teaching. It is recommended to use different LLMs than the primary Navigator LLM for this step."
+                "AI Align AI (AAA) is a technique that iteratively improves the quality and coherence of generated outputs by using multiple LLMs for co-teaching and self-teaching. Enabling AAA will enhance the overall quality of the synthetic data, but it may slow down the generation process."
             )
 
-            co_teach_llms = []
-            for model in nl_models:
-                if model != navigator_llm:
-                    if st.checkbox(model, value=True, key=f"checkbox_{model}"):
-                        co_teach_llms.append(model)
-                else:
-                    if st.checkbox(model, value=False, key=f"checkbox_{model}"):
-                        co_teach_llms.append(model)
+            use_aaa = st.checkbox(
+                "Use AI Align AI (AAA)",
+                value=st.session_state.get("use_aaa", True),
+                help="Enable or disable the use of AI Align AI.",
+            )
+            st.session_state.use_aaa = use_aaa
 
-            st.session_state.co_teach_llms = co_teach_llms
+            co_teach_llms = []  # Initialize co_teach_llms with an empty list
+
+            if use_aaa:
+                st.markdown("#### Navigator Co-teaching LLMs")
+                st.write(
+                    "Select additional Navigator LLMs for co-teaching in AAA. It is recommended to use different LLMs than the primary Navigator LLM for this step."
+                )
+
+                for model in nl_models:
+                    if model != navigator_llm:
+                        if st.checkbox(model, value=True, key=f"checkbox_{model}"):
+                            co_teach_llms.append(model)
+                    else:
+                        if st.checkbox(model, value=False, key=f"checkbox_{model}"):
+                            co_teach_llms.append(model)
+                st.session_state.co_teach_llms = co_teach_llms
+
+            st.markdown("---")
+            st.markdown("### Format Prompts")
 
             instruction_format_prompt = st.text_area(
                 "Instruction Format Prompt",
@@ -308,31 +333,44 @@ def main():
             )
             st.session_state.response_format_prompt = response_format_prompt
 
-        with st.expander("Download Configuration", expanded=False):
+        with st.expander("Download SDK Code", expanded=False):
             config_text = f"""
-        input_fields = {st.session_state.selected_fields}
-        output_instruction_field = "{output_instruction_field}"
-        output_response_field = "{output_response_field}"
-        num_instructions = {num_instructions}
-        num_responses = {num_responses}
-        temperature = {temperature}
-        max_tokens_instruction = {max_tokens_instruction}
-        max_tokens_response = {max_tokens_response}
-        navigator_tabular = "{navigator_tabular}"
-        navigator_llm = "{navigator_llm}"
-        co_teach_llms = {co_teach_llms}
-        instruction_format_prompt = "{instruction_format_prompt}"
-        response_format_prompt = "{response_format_prompt}"
+        # Create the data augmentation configuration
+        config = DataAugmentationConfig(
+            input_fields={st.session_state.selected_fields},
+            output_instruction_field="{output_instruction_field}",
+            output_response_field="{output_response_field}",
+            num_instructions={num_instructions},
+            num_responses={num_responses},
+            temperature={temperature},
+            max_tokens_instruction={max_tokens_instruction},
+            max_tokens_response={max_tokens_response},
+            api_key=YOUR_GRETEL_API_KEY,
+            navigator_tabular="{navigator_tabular}",
+            navigator_llm="{navigator_llm}",
+            co_teach_llms={co_teach_llms},
+            instruction_format_prompt="{instruction_format_prompt}",
+            response_format_prompt="{response_format_prompt}"
+        )
+
+        # Create the data augmenter and perform augmentation
+        augmenter = DataAugmenter(
+            df,
+            config,
+            use_aaa={use_aaa},
+            output_file="results.csv",
+            verbose=True,
+        )
+        new_df = augmenter.augment()
         """
-
             st.code(config_text, language="python")
-
             st.download_button(
-                label="Download DataAugmentationConfig",
+                label="Download SDK Code",
                 data=config_text,
-                file_name="data_augmentation_config.txt",
+                file_name="data_augmentation_code.py",
                 mime="text/plain",
             )
+
 
         start_stop_container = st.empty()
 
@@ -340,44 +378,32 @@ def main():
         with col1:
             start_button = st.button("🚀 Start")
         with col2:
-            stop_button = st.button("Stop")
+            stop_button = st.button("🛑 Stop")
 
         if start_button:
             with st.expander("Augmentation Results", expanded=True):
                 st.subheader("Augmentation Results")
-
                 progress_bar = st.progress(0)
-
                 tab1, tab2 = st.tabs(["Augmented Data", "Logs"])
-
                 with tab1:
                     augmented_data_placeholder = st.empty()
                     st.info(
                         "Click on the 'Logs' tab to see and debug real-time logging for each record as it is generated by the agents."
                     )
-
                 with tab2:
                     log_container = st.empty()
                     logs = []
-
+                    max_log_lines = 50
                 def custom_log_handler(msg):
                     nonlocal logs
-                    # logs.insert(0, msg)
                     logs.append(msg)
+                    if len(logs) > max_log_lines:
+                        logs = logs[-max_log_lines:]
                     log_text = "\n".join(logs)
                     log_container.text(log_text)
-
-                    if (
-                        "🆕 Starting the process of generating a new augmented record."
-                        in msg
-                    ):
-                        time.sleep(3)
-                        logs.clear()
-
                 handler = StreamlitLogHandler(custom_log_handler)
                 logger = logging.getLogger("navigator_helpers")
                 logger.addHandler(handler)
-
                 config = DataAugmentationConfig(
                     input_fields=selected_fields,
                     output_instruction_field=output_instruction_field,
@@ -394,9 +420,8 @@ def main():
                     instruction_format_prompt=instruction_format_prompt,
                     response_format_prompt=response_format_prompt,
                 )
-
                 augmented_data = []
-
+                start_time = time.time()
                 with st.spinner("Generating synthetic data..."):
                     for index in range(num_records):
                         row = df.iloc[index]
@@ -407,26 +432,34 @@ def main():
                             output_file="results.csv",
                             verbose=True,
                         )
-
                         new_df = augmenter.augment()
                         augmented_data.append(new_df)
                         augmented_data_placeholder.subheader("Augmented Data")
                         augmented_data_placeholder.dataframe(
                             pd.concat(augmented_data, ignore_index=True)
                         )
-
-                        progress_bar.progress((index + 1) / num_records)
+                        progress = (index + 1) / num_records
+                        progress_bar.progress(progress)
+                        
+                        elapsed_time = time.time() - start_time
+                        records_processed = index + 1
+                        records_remaining = num_records - records_processed
+                        est_time_per_record = elapsed_time / records_processed if records_processed > 0 else 0
+                        est_time_remaining = est_time_per_record * records_remaining
+                        
+                        progress_text = f"Progress: {progress:.2%} | Records Processed: {records_processed} | Records Remaining: {records_remaining} | Est. Time per Record: {est_time_per_record:.2f}s | Est. Time Remaining: {est_time_remaining:.2f}s"
+                        progress_bar.text(progress_text)
+                        
                         time.sleep(0.1)
-
                 logger.removeHandler(handler)
                 st.success("Data augmentation completed!")
         if stop_button:
             st.warning("Augmentation stopped by the user.")
             st.stop()
-    else:
-        st.info(
-            "Please upload a file or select a dataset from Hugging Face to proceed."
-        )
+        else:
+            st.info(
+                "Please upload a file or select a dataset from Hugging Face to proceed."
+            )
 
 
 if __name__ == "__main__":
