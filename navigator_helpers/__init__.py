b8af33a24bc6dbb8a804bed267b4e5c70a157b47
Alexander Watson <zredlined@users.noreply.github.com>
2024-07-16 06:53:21 -0700

    black formatting

--- navigator_helpers/__init__.py
+++ navigator_helpers/__init__.py
@@ -1,4 +1,7 @@
 from .conversation_synthesizer import Conversation, ConversationSynthesizer
-from .data_synthesis import (InstructionResponseConfig, SingleTextConfig,
-                             StreamlitLogHandler)
+from .data_synthesis import (
+    InstructionResponseConfig,
+    SingleTextConfig,
+    StreamlitLogHandler,
+)
 from .training_data_synthesizer import TrainingDataSynthesizer
