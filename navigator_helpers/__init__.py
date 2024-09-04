e29c4d7bfa5ef9eb8545073198e84c60c9db9608
Johnny Greco <jgreco.ai@gmail.com>
2024-09-04 11:59:38 -0400

    fix messed up merge conflict

--- navigator_helpers/__init__.py
+++ navigator_helpers/__init__.py
@@ -11,13 +11,12 @@ except ImportError:
 
 from .content_validator import ContentValidator
 from .data_models import DataFieldDefinition, DataModelDefinition, GeneratorConfig
+from .evolutionary_data_generator import EvolDataGenerator
 from .evolutionary_strategies import get_prebuilt_evolutionary_strategies
-from .synthetic_data_generator import EvolDataGenerator
 from .tasks.text_to_code.config import smart_load_pipeline_config
 from .tasks.text_to_code.llm_suite import GretelLLMSuite
 from .tasks.text_to_code.pipeline import NL2CodePipeline
 from .tasks.text_to_code.task_suite import NL2CodeTaskSuite
-from .evolutionary_data_generator import EvolDataGenerator
 
 __all__ = [
     "DataFieldDefinition",
