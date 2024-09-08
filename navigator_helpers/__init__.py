6571c88e53a1c7e35e335f5e734cf0eee5f4528f
Alexander Watson <zredlined@users.noreply.github.com>
2024-09-08 16:46:32 -0700

    Update __init__.py

--- navigator_helpers/__init__.py
+++ navigator_helpers/__init__.py
@@ -2,6 +2,7 @@ from .content_validator import ContentValidator
 from .data_models import DataFieldDefinition, DataModelDefinition, GeneratorConfig
 from .evolutionary_strategies import get_prebuilt_evolutionary_strategies
 from .evolutionary_data_generator import EvolDataGenerator
+from .text_inference import TextInference
 
 __all__ = [
     "DataFieldDefinition",
@@ -9,5 +10,6 @@ __all__ = [
     "GeneratorConfig",
     "EvolDataGenerator",
     "ContentValidator",
+    "TextInference",
     "get_prebuilt_evolutionary_strategies",
 ]
