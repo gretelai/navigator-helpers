3822d55fc8d24c073487a367ab1177c033dbe7ed
nina-xu <nina.ning.xu@gmail.com>
2024-09-27 17:50:50 +0000

    add ruff for python

--- experiments/code_validation/python_parsers.py
+++ experiments/code_validation/python_parsers.py
@@ -1,6 +1,8 @@
 import ast
 import io
+import json
 import parso
+import subprocess
 
 from mypy import api
 from pyflakes.api import check
@@ -63,4 +65,58 @@ def is_valid_python_with_mypy(code_str: str) -> Tuple[bool, str]:
     else:
         return False, "Failed to run mypy"
 
-# TODO: Add ruff implementation
+def is_valid_python_with_ruff(code_str: str, level: str = 'error', ruff_rules: list = None) -> Tuple[bool, str]:
+
+    assert level in ['error', 'warning', 'custom'], "level should be either 'error' or 'warning' or 'custom'"
+    if level == 'custom':
+        assert isinstance(ruff_rules, list), "When level is 'custom', ruff_rules should be a list of strings"
+
+    def check_ruff(rules: list):
+        proc = subprocess.run(
+            ["ruff", "check", f"--select={','.join(rules)}", "--output-format=json", "-"],
+            input=code_str,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            encoding="utf-8",
+        )
+
+        result = json.loads(proc.stdout)
+        error_codes = set([issue['code'] for issue in result])
+        error_messages = set([issue['message'] for issue in result])
+
+        if proc.returncode == 0:
+            return True, None
+        elif proc.returncode == 1:
+            return False, f"{error_codes} {error_messages}"
+        elif proc.returncode == 2:
+            return None, "Failed to run ruff"
+
+    # Errors that will lead to runtime errors
+    rules_about_errors = [
+        "F821", "F822", "F823",    # Pyflakes: Undefined names, variables
+        "PLE",                     # Pylint: Errors
+    ]
+
+    # Issues that are highly likely to cause runtime errors
+    rules_about_issues = [
+        "F401", "F841", "F811",   # Pyflakes: Unused imports, variables
+        "ARG",                    # Flake8: Unused arguments
+        "B",                      # Bugbear: All bugbear issues which flag potential common bugs
+        "A",                      # Builtins: Avoid shadowing built-in names
+        "C901",                   # Complexity
+        "RET505", "RET506", "RET507", "RET508", # Flake8: Unreachable code
+        "FIX",                    # Flake8: Contains TODO or FIXME which indicates incomplete code
+    ]
+
+    if level == 'custom':
+        return check_ruff(ruff_rules)
+    
+    error_check, err_msg = check_ruff(rules_about_errors)
+    if error_check is False:
+        return False, err_msg
+    
+    if level == 'warning':
+        # If there are no obvious errors, check for issues
+        return check_ruff(rules_about_issues)
+    
+    return True, None
