ac3dc0d3a7dfcebd27f2d7635197c8e1b9e7b687
Piotr Mlocek <piotr@gretel.ai>
2024-09-27 12:44:40 -0700

    make style

--- experiments/code_validation/python_parsers.py
+++ experiments/code_validation/python_parsers.py
@@ -1,16 +1,18 @@
 import ast
 import io
+
+from typing import Tuple
+
 import parso
 
 from mypy import api
 from pyflakes.api import check
 from pyflakes.reporter import Reporter
-from typing import Tuple
 
 
 def is_valid_python_with_complie(code_str: str) -> Tuple[bool, str]:
     try:
-        compile(code_str, '<string>', 'exec')
+        compile(code_str, "<string>", "exec")
         return True, None
     except SyntaxError as e:
         # print(f"SyntaxError: {e}")
@@ -26,10 +28,11 @@ def is_valid_python_with_ast(code_str: str) -> Tuple[bool, str]:
         # print(f"SyntaxError: {e}")
         return False, str(e)
 
+
 def is_valid_python_with_pyflakes(code_str: str) -> Tuple[bool, str]:
     error_stream = io.StringIO()
     reporter = Reporter(error_stream, error_stream)
-    check(code_str, '<string>', reporter)
+    check(code_str, "<string>", reporter)
     errors = error_stream.getvalue()
     if errors:
         # print("Errors detected:")
@@ -37,6 +40,7 @@ def is_valid_python_with_pyflakes(code_str: str) -> Tuple[bool, str]:
         return False, errors
     else:
         return True, None
+
 
 def is_valid_python_with_parso(code_str: str) -> Tuple[bool, str]:
     # TODO: Too forgiving. See if there are settings to disable some checks.
@@ -51,16 +55,17 @@ def is_valid_python_with_parso(code_str: str) -> Tuple[bool, str]:
 
 
 def is_valid_python_with_mypy(code_str: str) -> Tuple[bool, str]:
-    result = api.run(['-c',  code_str, '--ignore-missing-imports'])
+    result = api.run(["-c", code_str, "--ignore-missing-imports"])
     stdout, stderr, exit_status = result
     if stdout:
         if exit_status == 0:
             return True, None
         else:
             # print("Mypy Output:")
-            # print(stdout)       
+            # print(stdout)
             return False, stdout
     else:
         return False, "Failed to run mypy"
+
 
 # TODO: Add ruff implementation
