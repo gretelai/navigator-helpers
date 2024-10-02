7ab887c5d79e1955a8bad6487f0a2e138036a067
Dhruv Nathawani <dhruv@gretel.ai>
2024-10-02 20:43:19 +0000

    test functions

--- navigator_helpers/tasks/evaluation/run_evalution.py
+++ navigator_helpers/tasks/evaluation/run_evalution.py
@@ -10,6 +10,14 @@ dataset = load_dataset("gretelai/synthetic_text_to_sql", split = "train")
 dataset_10 = dataset.select(range(10))
 
 dataset_10_pd = dataset_10.to_pandas()
-results = BaseEvaluationTaskSuite(dataset_10_pd).row_uniqueness()
+results_1 = BaseEvaluationTaskSuite(dataset_10_pd).row_uniqueness()
+results_2 = BaseEvaluationTaskSuite(dataset_10_pd).feature_cardinality()
+results_3 = BaseEvaluationTaskSuite(dataset_10_pd).feature_distribution()
+results_4 = BaseEvaluationTaskSuite(dataset_10_pd).num_words_per_record()
 
-print(results)
+import pdb; pdb.set_trace()
+
+print(results_1)
+print(results_2)
+print(results_3)
+print(results_4)
