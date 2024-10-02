def71aeb325fd5d6bd39758a0cff5219fd8554b1
Dhruv Nathawani <dhruv@gretel.ai>
2024-10-02 18:40:44 +0000

    test script

--- navigator_helpers/tasks/evaluation/run_evalution.py
+++ navigator_helpers/tasks/evaluation/run_evalution.py
@@ -2,3 +2,14 @@
 
 # Input arguments
 # - Path to the dataset
+
+from evaluation import BaseEvaluationTaskSuite
+from datasets import load_dataset
+
+dataset = load_dataset("gretelai/synthetic_text_to_sql", split = "train")
+dataset_10 = dataset.select(range(10))
+
+dataset_10_pd = dataset_10.to_pandas()
+results = BaseEvaluationTaskSuite(dataset_10_pd).row_uniqueness()
+
+print(results)
