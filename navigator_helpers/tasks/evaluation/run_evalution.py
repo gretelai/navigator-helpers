8bae91e1e3afc56dcba6cb864d78211c5ba5fd65
Lipika Ramaswamy <lipika.ramaswamy@gmail.com>
2024-10-03 19:21:35 +0000

    pair session oct 3

--- navigator_helpers/tasks/evaluation/run_evalution.py
+++ navigator_helpers/tasks/evaluation/run_evalution.py
@@ -3,21 +3,27 @@
 # Input arguments
 # - Path to the dataset
 
-from evaluation import BaseEvaluationTaskSuite
 from datasets import load_dataset
+from evaluation import BaseEvaluationTaskSuite
 
-dataset = load_dataset("gretelai/synthetic_text_to_sql", split = "train")
-dataset_10 = dataset.select(range(10))
+## TODO: add dict of datasets to test
+dataset = load_dataset("gretelai/synthetic_text_to_sql", split="train")
+dataset_10 = dataset.select(range(1000))
 
 dataset_10_pd = dataset_10.to_pandas()
 results_1 = BaseEvaluationTaskSuite(dataset_10_pd).row_uniqueness()
-results_2 = BaseEvaluationTaskSuite(dataset_10_pd).feature_cardinality()
-results_3 = BaseEvaluationTaskSuite(dataset_10_pd).feature_distribution()
-results_4 = BaseEvaluationTaskSuite(dataset_10_pd).num_words_per_record()
-
-import pdb; pdb.set_trace()
+# results_2 = BaseEvaluationTaskSuite(dataset_10_pd).feature_cardinality()
+# results_3 = BaseEvaluationTaskSuite(dataset_10_pd).feature_distribution()
+# results_4 = BaseEvaluationTaskSuite(dataset_10_pd).num_words_per_record()
 
 print(results_1)
-print(results_2)
-print(results_3)
-print(results_4)
+import pdb
+
+pdb.set_trace()
+
+# review specific records
+# print(dataset_10_pd.loc[results_1['non_semantically_unique_ids']])
+
+# print(results_2)
+# print(results_3)
+# print(results_4)
