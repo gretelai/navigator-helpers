d34d4ee97a3c78f4f728778e7928fa4fa6ba15dc
nina-xu <nina.ning.xu@gmail.com>
2024-10-03 17:06:22 +0000

    add llm as a critic

--- navigator_helpers/tasks/evaluation/run_evalution.py
+++ navigator_helpers/tasks/evaluation/run_evalution.py
@@ -2,18 +2,33 @@
 
 # Input arguments
 # - Path to the dataset
+from navigator_helpers.llms.llm_suite import GretelLLMSuite
 
-from evaluation import BaseEvaluationTaskSuite
+from evaluation import BaseEvaluationTaskSuite, NL2SQLEvaluationTaskSuite
 from datasets import load_dataset
 
 dataset = load_dataset("gretelai/synthetic_text_to_sql", split = "train")
 dataset_10 = dataset.select(range(10))
 
 dataset_10_pd = dataset_10.to_pandas()
-results_1 = BaseEvaluationTaskSuite(dataset_10_pd).row_uniqueness()
-results_2 = BaseEvaluationTaskSuite(dataset_10_pd).feature_cardinality()
-results_3 = BaseEvaluationTaskSuite(dataset_10_pd).feature_distribution()
-results_4 = BaseEvaluationTaskSuite(dataset_10_pd).num_words_per_record()
+# print(dataset_10_pd.columns)
+
+llm_suite = GretelLLMSuite()
+results_1 = BaseEvaluationTaskSuite(llm_suite, dataset_10_pd).row_uniqueness()
+results_2 = BaseEvaluationTaskSuite(llm_suite, dataset_10_pd).feature_cardinality()
+results_3 = BaseEvaluationTaskSuite(llm_suite, dataset_10_pd).feature_distribution()
+results_4 = BaseEvaluationTaskSuite(llm_suite, dataset_10_pd).num_words_per_record()
+
+dataset_1_pd = dataset.select(range(1)).to_pandas()
+results_5 = NL2SQLEvaluationTaskSuite(
+    llm_suite=llm_suite, dataset=dataset_1_pd, code_lang="sql"
+    ).llm_as_a_critic_evaluation(
+    instruction_col_name="sql_prompt", code_col_name="sql", context_col_name="sql_context"
+)
+
+results_6 = BaseEvaluationTaskSuite(llm_suite, dataset_1_pd).llm_as_a_critic_evaluation(
+    instruction_col_name="sql_prompt", code_col_name="sql"
+)
 
 import pdb; pdb.set_trace()
 
@@ -21,3 +36,5 @@ print(results_1)
 print(results_2)
 print(results_3)
 print(results_4)
+print(results_5)
+print(results_6)
--- navigator_helpers/tasks/prompt_templates/llm_as_a_judge.py
+++ navigator_helpers/tasks/prompt_templates/llm_as_a_judge.py
@@ -1,5 +1,92 @@
-llm_as_a_judge_template_dict = dict(
-    sql_quality_rubric="""\
+# What if there are multiple instruction columns?
+# What if there are multiple response columns?
+# What if there is an overall "system instruction" applied to every record? e.g. in a preference dataset, one column is expected to be better than the other
+
+# My overall thought is that this base rubric should be generic, and inclusive of any derived rubrics
+general_response_quality_rubric="""\
+You are a domain expert in X. Please act as an impartial judge and use the rubric below to grade the quality of the response based on the Instructions.
+
+Provide output in the form of the following JSON:
+{{
+    "relevance": {{"score": score, "reasoning": reasoning}}, 
+}}
+
+#### Instructions:
+{natural_language}
+
+#### Response:
+{code}
+
+#### Rubric:
+Relevance: Adherence to Instructions
+* Score = 4: Perfectly meets all specified requirements.
+* Score = 3: Meets most requirements with minor deviations.
+* Score = 2: Moderate deviation from the instructions.
+* Score = 1: Significant deviations from the instructions.
+* Score = 0: Does not adhere to the instructions.
+"""
+
+code_quality_rubric="""\
+You are a {coding_language}programming expert, bringing together expertise from across software engineering, data science, and algorithmic problem-solving.
+Take a deep breath and use the code quality rubric below to grade the quality of {coding_language}code generated based on the INSTRUCTIONS.
+Provide output in the form of the following JSON:
+{{{{
+    "relevance": {{{{"score": score, "reasoning": reasoning}}}},
+    "correctness": {{{{"score": score, "reasoning": reasoning}}}},
+    "readability": {{{{"score": score, "reasoning": reasoning}}}},
+    "efficiency": {{{{"score": score, "reasoning": reasoning}}}},
+    "convention": {{{{"score": score, "reasoning": reasoning}}}}
+}}}}
+
+#### INSTRUCTIONS
+The Generated {coding_language}Code should be generated in response to the Natural Language Prompt below
+
+Natural Language Prompt:
+{{natural_language}}
+
+Generated {coding_language}Code:
+```{coding_language_lower}
+{{code}}
+```
+
+#### {coding_language}Code Quality Rubric
+Relevance: Adherence to Instructions
+* Score = 4: Perfectly meets all specified requirements.
+* Score = 3: Meets most requirements with minor deviations.
+* Score = 2: Moderate deviation from the instructions.
+* Score = 1: Significant deviations from the instructions.
+* Score = 0: Does not adhere to the instructions.
+
+Correctness: Code Correctness (Does the {coding_language}code provide the correct result?)
+* Score = 4: The code executes flawlessly, producing accurate and complete results as per the requirement; logic perfectly aligns with intended operations.
+* Score = 3: The code produces correct results with negligible issues not affecting the main output; logic aligns well with intended outcomes.
+* Score = 2: The code produces mostly correct results but with minor inaccuracies or omissions; logic generally reflects intended operations.
+* Score = 1: The code executes but produces partially correct results, significant errors present; some logical discrepancies present.
+* Score = 0: The code does not execute or produces incorrect results; logic does not align with intended operations.
+
+Readability: Readability and Maintainability (Is the {coding_language}code easy to understand and maintain?)
+* Score = 4: The code is excellently formatted, follows {style} guidelines, is thoroughly commented, uses meaningful variable names, ensuring high readability and ease of maintenance; organizes complex logic well.
+* Score = 3: The code is well-formatted and commented, making it relatively easy to understand and maintain; uses descriptive names and organizes logic clearly.
+* Score = 2: The code is somewhat readable with basic formatting and some comments, but improvements are needed; needs better use of descriptive names and organization.
+* Score = 1: The code has minimal formatting and few comments, making it hard to understand; lacks meaningful names and organization.
+* Score = 0: The code is unreadable, with no attempt at formatting or commenting.
+
+Efficiency: Efficiency and Performance (Is the code optimized for performance?)
+* Score = 4: The solution is highly efficient, using appropriate data structures and algorithms; avoids unnecessary computations and optimizes for both time and space complexity.
+* Score = 3: The solution is efficient, with good use of {coding_language}built-in functions and libraries; minor areas for optimization.
+* Score = 2: The solution is moderately efficient, but misses some opportunities for optimization; uses some inefficient patterns.
+* Score = 1: The solution shows poor efficiency, with notable performance issues; lacks effective optimization techniques.
+* Score = 0: The solution is highly inefficient; overlooks fundamental optimization practices, resulting in significant performance issues.
+
+Convention: Convention and Best Practices (Does the code follow {coding_language}conventions and best practices?)
+* Score = 4: The code exemplifies {coding_language}coding principles, making excellent use of {coding_language_dash}specific features and idioms; follows all {style} guides.
+* Score = 3: The code closely follows {coding_language}conventions and adheres to many best practices; good use of {coding_language_dash}specific features.
+* Score = 2: The code generally follows {coding_language}conventions but has room for better alignment with {coding_language}best practices.
+* Score = 1: The code loosely follows {coding_language}conventions, with several deviations from best practices.
+* Score = 0: The code does not follow {coding_language}conventions or best practices, using non-{coding_language}coding approaches.
+"""
+
+sql_quality_rubric="""\
 You are a SQL data expert, bringing together expertise from across data analysis, data science and data engineering.
 Take a deep breath and use the data quality rubric below to grade the quality of SQL generated based on INSTRUCTIONS and CONTEXT.
 Provide output in the form of a following JSON:
@@ -60,64 +147,21 @@ Standards: Compliance with Standards (Does the SQL query follow SQL standards an
 * Score = 2 The query generally follows SQL standards but has room for better alignment with best practices.
 * Score = 1 The query loosely follows SQL standards, with several deviations from best practices.
 * Score = 0 The query does not follow SQL standards or best practices, using deprecated or non-standard syntax.
-""",
-    python_quality_rubric="""\
-You are a Python programming expert, bringing together expertise from across software engineering, data science, and algorithmic problem-solving.
-Take a deep breath and use the code quality rubric below to grade the quality of Python code generated based on INSTRUCTIONS and CONTEXT.
-Provide output in the form of the following JSON:
-{{
-    "relevance": {{"score": score, "reasoning": reasoning}},
-    "correctness": {{"score": score, "reasoning": reasoning}},
-    "readability": {{"score": score, "reasoning": reasoning}},
-    "efficiency": {{"score": score, "reasoning": reasoning}},
-    "pythonic": {{"score": score, "reasoning": reasoning}}
-}}
+"""
 
-#### INSTRUCTIONS
-The Generated Python Code should be generated in response to the Natural Language Prompt below
-
-Natural Language Prompt:
-{natural_language}
-
-Generated Python Code
-```python
-{code}
-```
-
-#### Python Code Quality Rubric
-Relevance: Adherence to INSTRUCTIONS and CONTEXT
-* Score = 4: Perfectly meets all specified requirements.
-* Score = 3: Meets most requirements with minor deviations.
-* Score = 2: Moderate deviation from the instructions.
-* Score = 1: Significant deviations from the instructions.
-* Score = 0: Does not adhere to the instructions.
-
-Correctness: Code Correctness (Does the Python code provide the correct result?)
-* Score = 4: The code executes flawlessly, producing accurate and complete results as per the requirement; logic perfectly aligns with intended operations.
-* Score = 3: The code produces correct results with negligible issues not affecting the main output; logic aligns well with intended outcomes.
-* Score = 2: The code produces mostly correct results but with minor inaccuracies or omissions; logic generally reflects intended operations.
-* Score = 1: The code executes but produces partially correct results, significant errors present; some logical discrepancies present.
-* Score = 0: The code does not execute or produces incorrect results; logic does not align with intended operations.
-
-Readability: Readability and Maintainability (Is the Python code easy to understand and maintain?)
-* Score = 4: The code is excellently formatted, follows PEP 8 guidelines, is thoroughly commented, uses meaningful variable names, ensuring high readability and ease of maintenance; organizes complex logic well.
-* Score = 3: The code is well-formatted and commented, making it relatively easy to understand and maintain; uses descriptive names and organizes logic clearly.
-* Score = 2: The code is somewhat readable with basic formatting and some comments, but improvements are needed; needs better use of descriptive names and organization.
-* Score = 1: The code has minimal formatting and few comments, making it hard to understand; lacks meaningful names and organization.
-* Score = 0: The code is unreadable, with no attempt at formatting or commenting.
-
-Efficiency: Efficiency and Performance (Is the code optimized for performance?)
-* Score = 4: The solution is highly efficient, using appropriate data structures and algorithms; avoids unnecessary computations and optimizes for both time and space complexity.
-* Score = 3: The solution is efficient, with good use of Python's built-in functions and libraries; minor areas for optimization.
-* Score = 2: The solution is moderately efficient, but misses some opportunities for optimization; uses some inefficient patterns.
-* Score = 1: The solution shows poor efficiency, with notable performance issues; lacks effective optimization techniques.
-* Score = 0: The solution is highly inefficient; overlooks fundamental optimization practices, resulting in significant performance issues.
-
-Pythonic: Pythonic Code and Best Practices (Does the code follow Python conventions and best practices?)
-* Score = 4: The code exemplifies Pythonic principles, making excellent use of Python-specific features and idioms; follows all relevant PEPs.
-* Score = 3: The code closely follows Python conventions and adheres to many best practices; good use of Python features.
-* Score = 2: The code generally follows Python conventions but has room for better alignment with Pythonic practices.
-* Score = 1: The code loosely follows Python conventions, with several deviations from best practices.
-* Score = 0: The code does not follow Python conventions or best practices, using non-Pythonic approaches.
-""",
+llm_as_a_judge_template_dict = dict(
+    general_response_quality_rubric=general_response_quality_rubric,
+    # We can do a general code quality rubric for all languages as a fallback
+    # But we should typically expect a language name to be specified
+    general_code_quality_rubric=code_quality_rubric.format(
+        coding_language="", 
+        coding_language_lower="",
+        coding_language_dash="language-",
+        style="relevant style"), 
+    python_quality_rubric=code_quality_rubric.format(
+        coding_language="Python ", 
+        coding_language_lower="python",
+        coding_language_dash="Python-",
+        style="PEP 9"), 
+    sql_quality_rubric=sql_quality_rubric,
 )
