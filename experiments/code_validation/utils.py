bba6d0c0c7c1f37253fe32e7eb65a5029f24499d
nina-xu <nina.ning.xu@gmail.com>
2024-10-08 15:27:19 +0000

    fix some bugs in sql execution

--- experiments/code_validation/utils.py
+++ experiments/code_validation/utils.py
@@ -1,6 +1,16 @@
+import re
+
 def split_statements(query):
     # Split a string of multiple statements into a list of individual statements
     statements = [s.strip() for s in query.split(";")]
     statements = [s for s in statements if s != ""]
     statements = [s + ";" for s in statements if s[-1] != ";"]
     return statements
+
+def create_db_name(domain):
+    # Given a domain name, create a valid database name
+    db_name = domain.replace(" ", "_").replace("-", "_").lower()
+    # If the db_name starts with a digit, prepend "db_"
+    if bool(re.match(r"^\d", db_name)):
+        db_name = "db_" + db_name
+    return db_name
