68d4467ba614b57591dadd3433903f07246359e5
Dhruv Nathawani <dhruv@gretel.ai>
2024-10-08 18:27:21 +0000

    Clean up evaluation

--- navigator_helpers/tasks/evaluation/evaluation.py
+++ navigator_helpers/tasks/evaluation/evaluation.py
@@ -14,14 +14,14 @@ from navigator_helpers.tasks.text_to_code import utils
 llm_as_a_judge_prompts = load_prompt_template_suite("llm_as_a_judge")
 
 class BaseEvaluationTaskSuite(BaseTaskSuite):
-    # TODO: Add typing, docstrings
-    # Generic class for evaluation task suites
-    # Tests include:
-    # 1. Row uniqueness
-    # 2. Feature cardinality
-    # 3. Feature distribution
-    # 4. num_words per record
-    # 5. LLM-as-a-critic evaluation based on a generic dataset rubric (e.g. data quality, relevance, correctness, readability)
+    """
+    Base class for evaluation task suites, which include a set of generic evaluation tasks :
+        1. Row uniqueness
+        2. Feature cardinality
+        3. Feature distribution
+        4. num_words per record
+        5. LLM-as-a-critic evaluation based on a generic dataset rubric (e.g. data quality, relevance, correctness, readability)
+    """
 
     def __init__(self, llm_suite: GretelLLMSuite, dataset: pd.DataFrame) -> None:
         super().__init__(llm_suite)
@@ -35,7 +35,7 @@ class BaseEvaluationTaskSuite(BaseTaskSuite):
         )
         return prompt
     
-    def _eval_response_with_llm_as_judge(
+    def _eval_response_with_llm_as_a_judge(
         self,
         natural_language: str,
         code: str,
@@ -52,53 +52,33 @@ class BaseEvaluationTaskSuite(BaseTaskSuite):
         return scores
 
     def row_uniqueness(self):
-        # Test for brute force row uniqueness / semanantic uniqueness
-        # Input : Pandas dataset
-        # Output : Percentage of rows are unique,
-        # Percentage of rows are semantically unique
-        # Ids of rows that are not unique
-        # Ids of rows that are not unique semantically
-        # Assumptions : N/A
-        # Algorithm for row uniqueness:
-        # 1. df.unique on the whole dataset, if the length of the unique rows is equal to the length of the dataset, then all rows are unique
-        # 2. Group by contextual tags and then check for uniqueness using df.unique
-        # 4. Group by prompts and then check for uniqueness using df.unique
-        # 5. Check fuzzy match for other columns
-        # Improvements :
-        # Fuzzy matching
-        # Word-llama
-        # Embeddings to check for cosine similarity
-        # Example :
-        # Prompt 1 : "How to implement k-means clustering in Python?"
-        # Prompt 2 : "Can you implement k-means clustering in Python?"
-        # Example 2:
-        # Response 1 : "def square_root(num): ..."
-        # Response 2 : "def square_root_of(num): ..."
         """
-        Test for brute force row uniqueness / semantic uniqueness.
+        Evaluation test for brute force row uniqueness & semantic uniqueness using TF-IDF and cosine similarity.
         Returns:
             - Percentage of rows that are unique
             - Percentage of rows that are semantically unique (fuzzy match)
             - IDs of rows that are not unique
             - IDs of rows that are not semantically unique
+        Example for semantic duplicates:
+            - Prompt 1 : "How to implement k-means clustering in Python?"
+            - Prompt 2 : "Can you implement k-means clustering in Python?"
         """
-        # Row uniqueness based on exact match
+        # TODO: Try out https://github.com/dleemiller/WordLlama for semantic uniqueness
+
         total_rows = len(self.dataset)
-        # TODO: Is df.unique or drop_duplicates better?
+
+        # Brute Force row uniqueness by detecting duplicates
         unique_rows = self.dataset.drop_duplicates()
         non_unique_ids = self.dataset[self.dataset.duplicated()].index.tolist()
         percent_unique = len(unique_rows) / total_rows * 100
 
-        # Semantic uniqueness (not exact matches) using cosine similarity and TF-IDF
-        # TODO: Try out https://github.com/dleemiller/WordLlama
-        # TODO: Check if this is including text columns
+        # Semantic uniqueness using cosine similarity and TF-IDF
         text_columns = self.dataset.select_dtypes(include=[object])
         concatenated_text = text_columns.apply(
             lambda row: " ".join(row.values.astype(str)), axis=1
         )
         vectorizer = TfidfVectorizer().fit_transform(concatenated_text)
         vectors = vectorizer.toarray()
-
         cosine_sim = cosine_similarity(vectors)
         np.fill_diagonal(cosine_sim, 0)
         cosine_sim[np.tril_indices(cosine_sim.shape[0])] = 0
@@ -118,21 +98,25 @@ class BaseEvaluationTaskSuite(BaseTaskSuite):
         }
 
     def feature_cardinality(self):
-        # Test for feature cardinality
-        # Input : Pandas dataset
-        # Output : Returns a dictionary where each column is mapped to cardinality
-        # Algorithm for feature cardinality:
-        # 1. Calculate the percentage of unique values in each column using df.unique
-        # 2. TODO: Additional logging to show the distribution of unique values
-        # Related : https://github.com/Gretellabs/monogretel/blob/master/python/src/gretel_tabllm/astrolabe/expect/data.py#L117
+        """
+        Evaluation test for feature cardinality
+        Returns:
+            - Dictionary where each column is mapped to cardinality
+        """
+
+        # Calculate the percentage of unique values in each column
         cardinality = {
             col: self.dataset[col].nunique() / len(self.dataset)
             for col in self.dataset.columns
         }
         return cardinality
 
-    def simpson_di(self, data: dict):
-        """Given a hash { 'species': count } , returns the Simpson Diversity Index"""
+    def simpson_diversity_index(self, data: dict):
+        """
+        Given a hash { 'species': count }, returns the Simpson Diversity Index
+        Returns:
+            - Simpson Diversity Index in the range [0, 1), where higher value indicates higher diversity
+        """
 
         def p(n, N):
             """Relative abundance"""
@@ -142,32 +126,32 @@ class BaseEvaluationTaskSuite(BaseTaskSuite):
                 return float(n) / N
 
         N = sum(data.values())
-
-        return sum(p(n, N) ** 2 for n in data.values() if n != 0)
+        return 1 - sum(p(n, N) ** 2 for n in data.values() if n != 0)
 
     def feature_distribution(self):
-        # Test for feature distribution
-        # Input : Pandas dataset
-        # Output : Returns a dictionary where each column is mapped to a distribution
-        # Algorithm for feature distribution:
-        # 1. Calculate the distribution of values in each column using df.value_counts
-        # 2. Need to think about categorical, numerical and text columns
-        # 3. Example: For code column, we can take the distribution of code length
-        # Related : Look at HF dataset visualization
+        """
+        Evaluation test for feature distribution
 
+        Returns:
+            - Dictionary where each column is mapped to a distribution
+        """
+        # Algorithm for feature distribution:
+            # 1. Calculate the distribution of values in each column using df.value_counts
+            # 2. Need to think about categorical, numerical and text columns
+            # 3. Example: For code column, we can take the distribution of code length
+        # TODO: Implement something similar to HF dataset visualization
         # TODO: classify columns as categorical, numerical, text
+
         distribution = {}
         score = {}
         for col in self.dataset.columns:
-            if (
-                self.dataset[col].dtype == "object"
-            ):  ## and some condition for categorical
+            if self.dataset[col].dtype == "object":
                 col_value_counts = self.dataset[col].value_counts().to_dict()
-                gini_simpson_index = 1 - self.simpson_di(data=col_value_counts)
+                gini_simpson_index = self.simpson_diversity_index(data=col_value_counts)
                 distribution[col] = col_value_counts
                 score[col] = round(gini_simpson_index, 4)
             elif self.dataset[col].dtype in ["int64", "float64"]:
-                # TODO : Add histogram?
+                # TODO : Histogram Visualization
                 pass
             else:  ## text
                 # TODO : string length, word count, characters per word, etc. - use Text SQS functions?
@@ -175,19 +159,17 @@ class BaseEvaluationTaskSuite(BaseTaskSuite):
         return distribution, score
 
     def num_words_per_record(self):
-        # Test for number of words per record
-        # Input : Pandas dataset
-        # Output : Returns a dictionary where each record is mapped to the count of words
-        # Plan to use this private function for feature distribution
         """
-        Test for number of words per record in text columns.
-        Returns the average and distribution of word counts per record.
+        Evaluation test for number of words per record in text columns.
+        Returns:
+            - Average number of words per record
+            - Distribution of word counts per record
         """
+
         text_columns = self.dataset.select_dtypes(include=[object])
         word_counts = text_columns.map(
             lambda x: len(str(x).split()) if isinstance(x, str) else 0
         )
-        # TODO: Do we want a distribution of word counts per record?
         avg_words_per_record = word_counts.mean().mean()
 
         return {
@@ -195,78 +177,68 @@ class BaseEvaluationTaskSuite(BaseTaskSuite):
             "word_counts_per_column": word_counts.mean().to_dict(),
         }
 
-    def llm_as_a_critic_evaluation(self,instruction_col_name: str, code_col_name: str):
-        # LLM-as-a-critic evaluation based on code rubric
+    def llm_as_a_judge_evaluation(self, instruction_col_name: str, code_col_name: str, context_col_name: str):
+        """
+        Evaluation test for LLM-as-a-judge evaluation based on generic rubric
+        Returns:
+            - Average LLM-as-a-judge score for all records
+        """
+
         self.output_dataset = self.dataset.copy()
-        self.output_dataset["scores"] = self.dataset.apply(lambda row: self._eval_response_with_llm_as_judge(
+        self.output_dataset["scores"] = self.dataset.apply(lambda row: self._eval_response_with_llm_as_a_judge(
             natural_language=row[instruction_col_name], 
             code=row[code_col_name]
             ), axis=1)
         
-        # Calculate an overall score. Use average for now. Should revisit
+        # TODO: Explore a better way to aggregate scores than average 
+        # Calculate the average score for each record
         self.output_dataset["overall_score"] = self.output_dataset["scores"].apply(lambda x: np.mean([int(v) for k, v in x.items() if "score" in k]))
 
-        return {'llm_as_a_critic_score': self.output_dataset.overall_score.mean()}
-
-
-# Check if validation was performed and if that column exists in the dataset
-# Let the user know you are performing validation by logging
-# Evaluate only the valid records
-# For now, we have only code specific validation for python & SQL
-# Report scores only on valid records
-# Validation functions can be seperate but should be called from the evaluation task suite
+        return {'llm_as_a_judge_score': self.output_dataset.overall_score.mean()}
 
 
 class NL2CodeEvaluationTaskSuite(BaseEvaluationTaskSuite):
-    # Class for evaluation task suites for natural language to code tasks
-    # Tests include:
-    # 1. LLM-as-a-critic evaluation based on code rubric (e.g. code quality, correctness, readability)
-    # 1.1 Seperate rubrics for SQL and Python
-    # 2. Code syntax validation
+    """
+    Code evaluation class, which include a set of code specific evaluation tasks :
+        1. LLM-as-a-judge evaluation based on code rubric (e.g. code quality, correctness, readability)
+    """
 
-    def __init__(self, llm_suite: GretelLLMSuite, dataset: pd.DataFrame, code_lang: str = None) -> None:
+    def __init__(self, llm_suite: GretelLLMSuite, dataset: pd.DataFrame, code_lang: str = '') -> None:
         super().__init__(llm_suite, dataset)
         self.code_lang = code_lang
     
     def _get_llm_as_a_judge_prompt(self, natural_language: str, code: str, **kwargs) -> str:
         if self.code_lang == "sql":
             rubric = llm_as_a_judge_prompts.sql_quality_rubric
-            prompt = rubric(
+        elif self.code_lang == "python":
+            rubric = llm_as_a_judge_prompts.python_quality_rubric
+        else:
+            rubric = llm_as_a_judge_prompts.general_code_quality_rubric
+        prompt = rubric(
                 natural_language=natural_language, code=code, **kwargs
             )
-        else:
-            if self.code_lang == "python":
-                rubric = llm_as_a_judge_prompts.python_quality_rubric        
-            else:
-                rubric = llm_as_a_judge_prompts.general_code_quality_rubric
-            prompt = rubric(
-                natural_language=natural_language, code=code
-            )
         return prompt
-    
-    def code_syntax_validation(self):
-        # Test for code syntax validation
-        pass
 
 
 class NL2PythonEvaluationTaskSuite(NL2CodeEvaluationTaskSuite):
-    # Class for evaluation task suites for natural language to python tasks
-    # Tests include:
-    # 1. Linter-based scoring for python code
+    """
+    Python evaluation class, which include a set of python specific evaluation tasks
+    """
     def linter_based_scoring(self):
         # Validation test for linter-based scoring for python code
+        # TODO: Do we want to combine validation into the evaluation suite?
         pass
 
 
 class NL2SQLEvaluationTaskSuite(NL2CodeEvaluationTaskSuite):
-    # Class for evaluation task suites for natural language to SQL tasks
-    # Tests include:
-    # 1. Linter-based scoring for SQL code
+    """
+    SQL evaluation class, which include a set of SQL specific evaluation tasks
+    """
     
-    def llm_as_a_critic_evaluation(self, instruction_col_name: str, code_col_name: str, context_col_name: str):
+    def llm_as_a_judge_evaluation(self, instruction_col_name: str, code_col_name: str, context_col_name: str):
         # LLM-as-a-critic evaluation based on code rubric
         self.output_dataset = self.dataset.copy()
-        self.output_dataset["scores"] = self.dataset.apply(lambda row: self._eval_response_with_llm_as_judge(
+        self.output_dataset["scores"] = self.dataset.apply(lambda row: self._eval_response_with_llm_as_a_judge(
             natural_language=row[instruction_col_name], 
             code=row[code_col_name],
             sql_context=row[context_col_name],
@@ -274,8 +246,4 @@ class NL2SQLEvaluationTaskSuite(NL2CodeEvaluationTaskSuite):
         
         # Calculate an overall score. Use average for now. Should revisit
         self.output_dataset["overall_score"] = self.output_dataset["scores"].apply(lambda x: np.mean([int(v) for k, v in x.items() if "score" in k]))
-        return {'llm_as_a_critic_score': self.output_dataset.overall_score.mean()}
-
-    def linter_based_scoring(self):
-        # Test for linter-based scoring for SQL code
-        pass
+        return {'llm_as_a_judge_score': self.output_dataset.overall_score.mean()}
