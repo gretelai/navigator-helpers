7cc380e67cf159e41acd7bff0225eef8a935f9d4
Alexander Watson <zredlined@users.noreply.github.com>
2024-10-11 08:56:48 -0700

    Update __init__.py

--- navigator_helpers/__init__.py
+++ navigator_helpers/__init__.py
@@ -9,16 +9,21 @@ try:
 except ImportError:
     IN_COLAB = False
 
-from .config.generator import ConfigGenerator
+from .base_text_inference import BaseTextInference
+from .config_generator.config_generator import ConfigGenerator
 from .content_validator import ContentValidator
-from .data_models import ContextualTag, ContextualTags, DataField, DataModel
-from .evolutionary_data_generator import EvolDataGenerator
-from .evolutionary_strategies import DEFAULT_EVOLUTION_STRATEGIES
+from .data_models import (
+    ContextualTag,
+    ContextualTags,
+    DataField,
+    DataModel,
+    GenerationStrategy,
+)
 from .llms.llm_suite import GretelLLMSuite
 from .pipelines.config.utils import smart_load_pipeline_config
 from .pipelines.text_to_code import NL2CodePipeline
+from .synthetic_data_generator import SyntheticDataGenerator
 from .tasks.text_to_code.task_suite import NL2PythonTaskSuite, NL2SQLTaskSuite
-from .text_inference import TextInference
 from .utils.logging import setup_logger
 
 __all__ = [
@@ -26,9 +31,9 @@ __all__ = [
     "DataModel",
     "ContextualTags",
     "ContextualTag",
-    "EvolDataGenerator",
+    "ConfigGenerator",
+    "SyntheticDataGenerator",
     "ContentValidator",
-    "TextInference",
-    "DEFAULT_EVOLUTION_STRATEGIES",
+    "BaseTextInference",
     "setup_logger",
 ]
